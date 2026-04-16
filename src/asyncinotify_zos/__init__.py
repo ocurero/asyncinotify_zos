6c37e76b646f25a180a178941e0329f1c65efc95
Oscar Curero <oscar@curero.es>
2026-04-16 13:35:02 +0200

    make it fail if not running on z/OS

--- src/asyncinotify_zos/__init__.py
+++ src/asyncinotify_zos/__init__.py
@@ -1,11 +1,15 @@
+import ctypes
 import glob
 import os
-import ctypes
+import sys
+
 
 _here = os.path.dirname(__file__)
 matches = glob.glob(os.path.join(_here, "libzosinotify*.so"))
 
-if not matches:
+if sys.platform != 'zos':
+    raise NotImplementedError("asyncinotify_zos only works on z/OS")
+elif not matches:
     raise ImportError("libzosinotify shared library not found")
 
 ctypes.CDLL(matches[0], mode=ctypes.RTLD_GLOBAL)
